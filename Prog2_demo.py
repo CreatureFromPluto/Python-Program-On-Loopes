ddc5711a5b7ac451ae57934e1f7af21379107931
CreatureFromPluto <ankit.sinha776@gmail.com>
2020-09-29 19:48:46 +0530

    Added a new program

---
+++ Prog2_demo.py
@@ -0,0 +1,10 @@
+print("This is a demo code")
+for i in range(1,10):
+    print(i*i," ")
+
+phus phus
+tera whatsapp me message nhi jaa rha
+sun
+i am showing how to create a repo and add all the code there.
+okay?
+in github
